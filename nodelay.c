x42/nodelay.lv2

commit 33b7651dae0e55868a9a963b61bb3fe935f0d45a

Add "MicroDelay" variant
This is a single slider -10k..+10k samples, where negative values
indicate virtual latency, while positive values are a simple delay.

nodelay.c
@@ -29,15 +29,7 @@
 #define NDL_URI "http://gareus.org/oss/lv2/nodelay"
 
 #define MAXDELAY (192001)
-#define FADE_LEN (16)
-
-typedef enum {
-	NDL_DELAY   = 0,
-	NDL_REPORT  = 1,
-	NDL_LATENCY = 2,
-	NDL_INPUT   = 3,
-	NDL_OUTPUT  = 4
-} PortIndex;
+#define FADE_LEN (32)
 
 typedef struct {
 	float* delay;
@@ -54,6 +46,7 @@ typedef struct {
 	/* settings from previous cycle */
 	int p_delay;
 	int p_mode;
+	int p_fade;
 } NoDelay;
 
 static LV2_Handle
@@ -74,20 +67,43 @@ connect_port (LV2_Handle instance,
 {
 	NoDelay* self = (NoDelay*)instance;
 
-	switch ((PortIndex)port) {
-		case NDL_DELAY:
+	switch (port) {
+		case 0:
 			self->delay = data;
 			break;
-		case NDL_REPORT:
+		case 1:
 			self->report_latency = data;
 			break;
-		case NDL_LATENCY:
+		case 2:
 			self->latency = data;
 			break;
-		case NDL_INPUT:
+		case 3:
 			self->input = data;
 			break;
-		case NDL_OUTPUT:
+		case 4:
+			self->output = data;
+			break;
+	}
+}
+
+static void
+connect_port_micro (LV2_Handle instance,
+                    uint32_t   port,
+                    void*      data)
+{
+	NoDelay* self = (NoDelay*)instance;
+
+	switch (port) {
+		case 0:
+			self->delay = data;
+			break;
+		case 1:
+			self->latency = data;
+			break;
+		case 2:
+			self->input = data;
+			break;
+		case 3:
 			self->output = data;
 			break;
 	}
@@ -105,58 +121,105 @@ connect_port (LV2_Handle instance,
 #endif
 
 static void
+no_delay (NoDelay* self, uint32_t n_samples)
+{
+	float const* const input = self->input;
+	for (uint32_t pos = 0; pos < n_samples; ++pos) {
+		self->buffer[self->w_ptr] = input[pos];
+		INCREMENT_PTRS;
+	}
+	if (self->input != self->output) {
+		memcpy (self->output, self->input, sizeof (float) * n_samples);
+	}
+}
+
+static void
+update_read_pointer (NoDelay* self, int new_delay)
+{
+	self->r_ptr += self->c_dly - new_delay;
+	if (self->r_ptr < 0) {
+		self->r_ptr -= MAXDELAY * floor (self->r_ptr / (float)MAXDELAY);
+	}
+	self->r_ptr = self->r_ptr % MAXDELAY;
+	self->c_dly = new_delay;
+}
+
+static void
+run_delay (NoDelay* self, uint32_t n_samples, int delay)
+{
+	uint32_t           pos    = 0;
+	float const* const input  = self->input;
+	float* const       output = self->output;
+
+	if (self->c_dly != delay) {
+		const int fade_len = (n_samples >= (2 * FADE_LEN)) ? FADE_LEN : n_samples / 2;
+
+		/* fade out */
+		for (; pos < fade_len; ++pos) {
+			self->buffer[self->w_ptr] = input[pos];
+			/* when latency has changed last cycle, and this cycles
+			 * adds a delay, there's alreay an active fade */
+			if (self->p_fade) {
+				output[pos] = 0;
+			} else {
+				const float gain = (float)(fade_len - pos) / (float)fade_len;
+				output[pos]      = self->buffer[self->r_ptr] * gain;
+			}
+			INCREMENT_PTRS;
+		}
+
+		/* update read pointer */
+		update_read_pointer (self, delay);
+
+		/* fade in */
+		for (; pos < 2 * fade_len; ++pos) {
+			const float gain          = (float)(pos - fade_len) / (float)fade_len;
+			self->buffer[self->w_ptr] = input[pos];
+			output[pos]               = self->buffer[self->r_ptr] * gain;
+			;
+			INCREMENT_PTRS;
+		}
+		self->p_fade = 0;
+	}
+
+	for (; pos < n_samples; ++pos) {
+		self->buffer[self->w_ptr] = input[pos];
+		output[pos]               = self->buffer[self->r_ptr];
+		INCREMENT_PTRS;
+	}
+}
+
+static void
+process (NoDelay* self, uint32_t n_samples, int delay)
+{
+	if (delay > 0 || self->c_dly > 0) {
+		run_delay (self, n_samples, delay);
+	} else {
+		no_delay (self, n_samples);
+		if (self->c_dly != delay) {
+			update_read_pointer (self, delay);
+		}
+	}
+}
+
+static void
 run (LV2_Handle instance, uint32_t n_samples)
 {
 	NoDelay* self = (NoDelay*)instance;
 
-	uint32_t           pos        = 0;
-	const float        delay_ctrl = MAX (0, MIN ((MAXDELAY - 1), *(self->delay)));
-	int                mode       = rint (*self->report_latency);
-	float const* const input      = self->input;
-	float* const       output     = self->p_mode >= 2 ? NULL : self->output;
-	int                delay      = self->p_delay;
+	const float delay_ctrl = MAX (0, MIN ((MAXDELAY - 1), *(self->delay)));
+	int         mode       = rint (*self->report_latency);
+	int         delay      = self->p_delay;
 
 	/* First report new latency to host. Only apply the change in the next cycle
 	 * after the host has updated the laency */
 	self->p_mode  = mode;
 	self->p_delay = rintf (delay_ctrl);
 
-	if (!output && self->input != self->output) {
-		memcpy (self->output, self->input, sizeof (float) * n_samples);
-	}
+	/* process using p_mode and fade c_dly -> delay */
+	process (self, n_samples, delay);
 
-	if (self->c_dly != delay) {
-		const int fade_len = (n_samples >= FADE_LEN) ? FADE_LEN : n_samples / 2;
-
-		/* fade out */
-		for (; pos < fade_len; ++pos) {
-			const float gain          = (float)(fade_len - pos) / (float)fade_len;
-			self->buffer[self->w_ptr] = input[pos];
-			if (output) {
-				output[pos] = self->buffer[self->r_ptr] * gain;
-			}
-			INCREMENT_PTRS;
-		}
-
-		/* update read pointer */
-		self->r_ptr += self->c_dly - delay;
-		if (self->r_ptr < 0) {
-			self->r_ptr -= MAXDELAY * floor (self->r_ptr / (float)MAXDELAY);
-		}
-		self->r_ptr = self->r_ptr % MAXDELAY;
-		self->c_dly = delay;
-
-		/* fade in */
-		for (; pos < 2 * fade_len; ++pos) {
-			const float gain          = (float)(pos - fade_len) / (float)fade_len;
-			self->buffer[self->w_ptr] = input[pos];
-			if (output) {
-				output[pos] = self->buffer[self->r_ptr] * gain;
-			}
-			INCREMENT_PTRS;
-		}
-	}
-
+	/* report latency */
 	switch (mode) {
 		case 3:
 		case 0:
@@ -169,13 +232,51 @@ run (LV2_Handle instance, uint32_t n_samples)
 			*(self->latency) = (float)self->c_dly;
 			break;
 	}
+}
 
-	for (; pos < n_samples; ++pos) {
-		self->buffer[self->w_ptr] = input[pos];
-		if (output) {
-			output[pos] = self->buffer[self->r_ptr];
+static void
+run_micro (LV2_Handle instance, uint32_t n_samples)
+{
+	NoDelay* self = (NoDelay*)instance;
+
+	const int delay_ctrl = MAX (-10000, MIN ((MAXDELAY - 1), rintf (*(self->delay))));
+	const int delay      = delay_ctrl >= 0 ? delay_ctrl : 0;
+	self->p_mode         = delay_ctrl >= 0 ? 0 : 2;
+
+	/* process using p_mode */
+	process (self, n_samples, delay);
+
+	/* fade in from previous cycle's fade-out (after a latency change) */
+	if (self->p_fade && delay == self->c_dly) {
+		float* const output   = self->output;
+		const int    fade_len = (n_samples >= (2 * FADE_LEN)) ? FADE_LEN : n_samples / 2;
+		for (uint32_t pos = 0; pos < fade_len; ++pos) {
+			const float gain = pos / (float)fade_len;
+			output[pos] *= gain;
 		}
-		INCREMENT_PTRS;
+	}
+
+	self->p_fade = 0;
+
+	/* latency changes, fade out at end, fade-in next cycle */
+	if (delay_ctrl != self->p_delay && delay_ctrl < 0) {
+		float* const output   = self->output;
+		const int    fade_len = (n_samples >= FADE_LEN) ? FADE_LEN : n_samples;
+		uint32_t     pos      = n_samples - fade_len;
+		for (uint32_t cnt = 0; cnt < fade_len; ++pos, ++cnt) {
+			const float gain = (float)(fade_len - cnt) / (float)fade_len;
+			output[pos] *= gain;
+		}
+		self->p_fade = 1;
+	}
+
+	self->p_delay = delay_ctrl;
+
+	/* report latency */
+	if (delay_ctrl >= 0) {
+		*(self->latency) = 0.f;
+	} else {
+		*(self->latency) = -delay_ctrl;
 	}
 }
 
@@ -191,12 +292,23 @@ extension_data (const char* uri)
 	return NULL;
 }
 
-static const LV2_Descriptor descriptor = {
+static const LV2_Descriptor descriptor0 = {
 	NDL_URI,
 	instantiate,
 	connect_port,
 	NULL,
 	run,
+	NULL,
+	cleanup,
+	extension_data
+};
+
+static const LV2_Descriptor descriptor1 = {
+	NDL_URI "#micro",
+	instantiate,
+	connect_port_micro,
+	NULL,
+	run_micro,
 	NULL,
 	cleanup,
 	extension_data
@@ -214,7 +326,9 @@ lv2_descriptor (uint32_t index)
 {
 	switch (index) {
 		case 0:
-			return &descriptor;
+			return &descriptor0;
+		case 1:
+			return &descriptor1;
 		default:
 			return NULL;
 	}

manifest.ttl.in
@@ -5,3 +5,8 @@
 	a lv2:Plugin ;
 	lv2:binary <nodelay@LIB_EXT@>  ;
 	rdfs:seeAlso <nodelay.ttl> .
+
+<http://gareus.org/oss/lv2/nodelay#micro>
+	a lv2:Plugin ;
+	lv2:binary <nodelay@LIB_EXT@>  ;
+	rdfs:seeAlso <nodelay.ttl> .

nodelay.ttl.in
@@ -1,9 +1,10 @@
+@prefix bufsz: <http://lv2plug.in/ns/ext/buf-size#> .
 @prefix doap: <http://usefulinc.com/ns/doap#> .
 @prefix foaf: <http://xmlns.com/foaf/0.1/> .
 @prefix lv2: <http://lv2plug.in/ns/lv2core#> .
 @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
-@prefix units: <http://lv2plug.in/ns/extensions/units#> .
 @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+@prefix units: <http://lv2plug.in/ns/extensions/units#> .
 
 <http://gareus.org/rgareus#me>
 	a foaf:Person ;
@@ -11,19 +12,21 @@
 	foaf:mbox <mailto:robin@gareus.org> ;
 	foaf:homepage <http://gareus.org/> .
 
+<http://ardour.org/lv2/ext#inlineMixerControl> a lv2:PortProperty .
+
 <http://gareus.org/oss/lv2/nodelay>
 	a lv2:Plugin, doap:Project, lv2:UtilityPlugin;
 	doap:license <http://usefulinc.com/doap/licenses/gpl> ;
 	doap:maintainer <http://gareus.org/rgareus#me> ;
 	doap:name "No Delay Line";
 	@VERSION@
-	lv2:optionalFeature lv2:hardRTCapable ;
+	lv2:optionalFeature lv2:hardRTCapable, bufsz:coarseBlockLength ;
 	lv2:port [
 		a lv2:InputPort ,
 			lv2:ControlPort ;
 		lv2:index 0 ;
 		lv2:symbol "delay" ;
-		lv2:name "delay";
+		lv2:name "Delay Time";
 		lv2:default 0 ;
 		lv2:minimum 0 ;
 		lv2:maximum 192000 ;
@@ -76,4 +79,50 @@
 		lv2:name "Out"
 	] ;
 	rdfs:comment "Artificial Latency - nodelay is a simple audio delay-line that can report its delay as latency. The effect should be transparent when used with a LV2 host that implements latency compensation." ;
+	.
+
+<http://gareus.org/oss/lv2/nodelay#micro>
+	a lv2:Plugin, doap:Project, lv2:UtilityPlugin;
+	doap:license <http://usefulinc.com/doap/licenses/gpl> ;
+	doap:maintainer <http://gareus.org/rgareus#me> ;
+	doap:name "Micro Delay Line";
+	@VERSION@
+	lv2:optionalFeature lv2:hardRTCapable, bufsz:coarseBlockLength ;
+	lv2:port [
+		a lv2:InputPort ,
+			lv2:ControlPort ;
+		lv2:index 0 ;
+		lv2:symbol "delay" ;
+		lv2:name "Delay Time";
+		lv2:default 0 ;
+		lv2:minimum -9600 ;
+		lv2:maximum  9600 ;
+		lv2:portProperty lv2:integer;
+		lv2:portProperty <http://ardour.org/lv2/ext#inlineMixerControl>;
+		units:unit units:frame;
+		rdfs:comment "Negative values indicate artificial latency (reported to the host), positive values delay the signal." ;
+	] , [
+		a lv2:OutputPort,
+			lv2:ControlPort ;
+		lv2:name "latency" ;
+		lv2:index 1 ;
+		lv2:symbol "latency" ;
+		lv2:minimum 0 ;
+		lv2:maximum 9600 ;
+		lv2:portProperty lv2:reportsLatency, lv2:integer;
+		units:unit units:frame;
+	] , [
+		a lv2:AudioPort ,
+			lv2:InputPort ;
+		lv2:index 2 ;
+		lv2:symbol "in" ;
+		lv2:name "In"
+	] , [
+		a lv2:AudioPort ,
+			lv2:OutputPort ;
+		lv2:index 3 ;
+		lv2:symbol "out" ;
+		lv2:name "Out"
+	] ;
+	rdfs:comment "Delayline and Artificial Latency." ;
 	.

commit b5250d0192a83293a018f3f65dadffe4aef53033

Hotfix for delay-mode (regression in 33b7651d)

nodelay.c
@@ -207,14 +207,14 @@ run (LV2_Handle instance, uint32_t n_samples)
 
 	const float delay_ctrl = MAX (0, MIN (DELAY_MASK, *(self->delay)));
 	int         mode       = rint (*self->report_latency);
-	int         delay      = self->p_delay;
+	int         delay      = self->p_mode < 2 ? self->p_delay : 0;
 
 	/* First report new latency to host. Only apply the change in the next cycle
-	 * after the host has updated the laency */
+	 * after the host has updated the latency */
 	self->p_mode  = mode;
 	self->p_delay = rintf (delay_ctrl);
 
-	/* process using p_mode and fade c_dly -> delay */
+	/* process fade c_dly -> delay */
 	process (self, n_samples, delay);
 
 	/* report latency */
@@ -239,9 +239,7 @@ run_micro (LV2_Handle instance, uint32_t n_samples)
 
 	const int delay_ctrl = MAX (-10000, MIN (DELAY_MASK, rintf (*(self->delay))));
 	const int delay      = delay_ctrl >= 0 ? delay_ctrl : 0;
-	self->p_mode         = delay_ctrl >= 0 ? 0 : 2;
 
-	/* process using p_mode */
 	process (self, n_samples, delay);
 
 	/* fade in from previous cycle's fade-out (after a latency change) */